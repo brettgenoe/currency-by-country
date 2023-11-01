a0113e7f94a18d2701ed5097b153b5fd7a490646
Brett <brettgenoe@gmail.com>
2023-11-01 15:53:42 -0700

    added boiler plate and map

---
+++ client/src/pages/header/header.js
@@ -0,0 +1,16 @@
+import './header.scss'
+
+
+const Header = () => {
+
+    return (
+        <>
+            <header className="header">
+                <h1 className="header__title" >Welcome to C by C</h1>
+            </header>
+        </>
+    )
+
+}
+
+export default Header
