d9d189fcee5ae49878502037dcc01c671cbfa629
Nate Paradiso <ipfcolo05@gmail.com>
2023-11-01 16:06:25 -0700

    country data get request

--- server/server.js
+++ server/server.js
@@ -7,18 +7,17 @@ const axios = require("axios");
 
 app.use(express.json());
 
-console.log("running")
-
-
 app.use(cors({ origin: "http://localhost:3000" }));
 
 
-app.get("/currency", async (req, res) => {
+app.get("/currency-cad", async (req, res) => {
 
-    url = "https://v6.exchangerate-api.com/v6/54d79f5447761709d2c1ff6b/latest/USD"
+    url = "https://v6.exchangerate-api.com/v6/54d79f5447761709d2c1ff6b/latest/EUR"
     try{
         const response = await axios.get(`${url}`);
         const currencyData = response.data;
+        // const parsedCurrencyData = JSON.parse(currencyData);
+
         res.json(currencyData);
         
 
@@ -27,9 +26,26 @@ app.get("/currency", async (req, res) => {
     res.status(500).json({ error: "failed to fetch data" });
     }
 
+
 });
+app.get("/country-data", async (req, res) => {
+
+    url = "https://restcountries.com/v3.1/all"
+    try{
+        const response = await axios.get(`${url}`);
+        const countryData = response.data;
+        // const parsedCurrencyData = JSON.parse(countryData);
+
+        res.json(countryData);
+        
+
+    } catch (error) {
+   
+    res.status(500).json({ error: "failed to fetch data" });
+    }
 
 
+});
 
 
 app.listen(8080, () => {
