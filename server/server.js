986ddd36ad8a20892411b3eb6ca4dd2db5606c62
aent <alintssr@gmail.com>
2023-11-02 13:51:29 -0400

    fixed header flags orientation

--- server/server.js
+++ server/server.js
@@ -10,21 +10,21 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
 
 
-app.get("/currency-usd", async (req, res) => {
+// app.get("/currency-usd", async (req, res) => {
 
-    url = "https://v6.exchangerate-api.com/v6/54d79f5447761709d2c1ff6b/latest/USD"
-    try{
+//     url = "https://v6.exchangerate-api.com/v6/54d79f5447761709d2c1ff6b/latest/USD"
+//     try{
 
-        const response = await axios.get(`${url}`);
-        const currencyData = response.data;
-        // const parsedCurrencyData = JSON.parse(currencyData);
-        res.json(currencyData);
+//         const response = await axios.get(`${url}`);
+//         const currencyData = response.data;
+//         // const parsedCurrencyData = JSON.parse(currencyData);
+//         res.json(currencyData);
 
-    } catch (error) {
+//     } catch (error) {
 
-        res.status(500).json({ error: "failed to fetch data" });
-    }
-});
+//         res.status(500).json({ error: "failed to fetch data" });
+//     }
+// });
 
 
 app.get("/country-data", async (req, res) => {
