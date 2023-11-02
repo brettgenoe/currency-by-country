6be9554069a7c15e04c0564e049ad1329050f8fd
Nate Paradiso <ipfcolo05@gmail.com>
2023-11-01 21:54:13 -0700

    new api request and json file

--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+const { v4: uuid } = require("uuid");
+
 
 
 
@@ -10,24 +14,23 @@ app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
 
 
-app.get("/currency-cad", async (req, res) => {
+app.get("/currency-usd", async (req, res) => {
 
-    url = "https://v6.exchangerate-api.com/v6/54d79f5447761709d2c1ff6b/latest/EUR"
+    url = "https://v6.exchangerate-api.com/v6/54d79f5447761709d2c1ff6b/latest/USD"
     try{
         const response = await axios.get(`${url}`);
         const currencyData = response.data;
         // const parsedCurrencyData = JSON.parse(currencyData);
-
         res.json(currencyData);
-        
-
     } catch (error) {
    
     res.status(500).json({ error: "failed to fetch data" });
     }
-
-
 });
+
+
+
+
 app.get("/country-data", async (req, res) => {
 
     url = "https://restcountries.com/v3.1/all"
@@ -35,19 +38,60 @@ app.get("/country-data", async (req, res) => {
         const response = await axios.get(`${url}`);
         const countryData = response.data;
         // const parsedCurrencyData = JSON.parse(countryData);
-
         res.json(countryData);
-        
-
     } catch (error) {
-   
     res.status(500).json({ error: "failed to fetch data" });
     }
+});
 
 
+
+app.get("/country", async (req, res) => {
+    const url = "https://restcountries.com/v3.1/all";
+
+    try {
+        const response = await axios.get(url);
+        const countryData = response.data;
+        const singleCountry = countryData.find(country => country.cca3 === "GBR");
+        
+        if (singleCountry) {
+            const selectedCountryData = {
+                id: uuid(),
+                countryName: singleCountry.name.common,
+                currency: singleCountry.currencies,
+                capitol: singleCountry.capital,
+                flag: singleCountry.flag,
+                population: singleCountry.population,
+                googleMapLink: singleCountry.maps?.googleMaps,
+            };
+            const filePath = path.join(__dirname, "data", "data.json");
+            fs.readFile(filePath, "utf-8", (readError, existingData) => {
+                if (readError) {
+                    console.error(readError);
+                    return res.status(500).json({ error: "Failed to read data file" });
+                }
+                const existingDataObj = JSON.parse(existingData);
+                const updatedData = { ...existingDataObj, selectedCountryData };
+                fs.writeFile(filePath, JSON.stringify(updatedData, null, 2), (writeError) => {
+                    if (writeError) {
+                        console.error(writeError);
+                        return res.status(500).json({ error: "Failed to write data to file" });
+                    }
+                    console.log("data.json updated correctly");
+                    res.json(selectedCountryData);
+                });
+            });
+        } else {
+            res.status(404).json({ error: "COUNTRY data not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch data" });
+    }
 });
 
 
 app.listen(8080, () => {
-  console.log("Listening on port 8080");
-});
+    console.log("Listening on port 8080");
+  });
+  
+  
